c90ee367a360182914b7ad483a563b55e7dab36d
Svetlin Ralchev <iamralch@users.noreply.github.com>
2023-09-23 13:37:38 +0400

    Introduce ReflectionService v1
    Signed-off-by: Svetlin Ralchev <iamralch@users.noreply.github.com>

---
+++ proto/grpc/reflection/v1/reflectionv1sdk/reflection.core.go
@@ -0,0 +1,32 @@
+package reflectionv1sdk
+
+import (
+	grpcreflect "connectrpc.com/grpcreflect"
+)
+
+// Reflector implements the underlying logic for gRPC's protobuf server reflection.
+// They're configurable, so they can support straightforward process-local reflection or more complex proxying.
+type Reflector = grpcreflect.Reflector
+
+// A Reflector Namer lists the fully-qualified Protobuf service names available for reflection
+// (for example, "acme.user.v1.UserService"). Namers must be safe to call concurrently.
+type ReflectorNamer = grpcreflect.Namer
+
+// NewReflector constructs a highly configurable Reflector:
+// it can serve a dynamic list of services, proxy reflection requests to other backends, or use an alternate source of extension information.
+var NewReflector = grpcreflect.NewReflector
+
+var _ ReflectorNamer = NameCollection{}
+
+// NameCollection is a list of names
+type NameCollection []string
+
+// WithName returns a NameCollection
+func WithName(name ...string) NameCollection {
+	return NameCollection(name)
+}
+
+// Names returns the names
+func (c NameCollection) Names() []string {
+	return c
+}
---
+++ proto/grpc/reflection/v1alpha/reflectionv1alphasdk/reflection.grpc.go
@@ -0,0 +1,34 @@
+package reflectionv1alphasdk
+
+import (
+	connect "connectrpc.com/connect"
+	grpcreflect "connectrpc.com/grpcreflect"
+	interceptor "github.com/connect-sdk/interceptor"
+	middleware "github.com/connect-sdk/middleware"
+	chi "github.com/go-chi/chi/v5"
+)
+
+// ReflectionServiceController represents an instance for grpc.reflection.v1alpha.ReflectionServiceHandler handler.
+type ReflectionServiceHandler struct {
+	// Reflector implements the underlying logic for gRPC's protobuf server reflection.
+	Reflector *Reflector
+}
+
+// Mount mounts the controller to a given router.
+func (x *ReflectionServiceHandler) Mount(r chi.Router) {
+	var options []connect.HandlerOption
+	// prepare the options
+	options = append(options, interceptor.WithTracer())
+	options = append(options, interceptor.WithLogger())
+	options = append(options, interceptor.WithRecovery())
+	options = append(options, interceptor.WithValidator())
+
+	r.Group(func(r chi.Router) {
+		// mount the middleware
+		r.Use(middleware.WithLogger())
+		// create the handler
+		path, handler := grpcreflect.NewHandlerV1Alpha(x.Reflector, options...)
+		// mount the handler
+		r.Mount(path, handler)
+	})
+}
